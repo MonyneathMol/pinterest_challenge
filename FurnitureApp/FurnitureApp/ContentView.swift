d0e06f306698e3da12322ac613a5a9e236be6749
Monyneath Mol <monyneath.mol@okoone.com>
2022-04-19 23:23:41 +0700

    continue search component

--- FurnitureApp/FurnitureApp/ContentView.swift
+++ FurnitureApp/FurnitureApp/ContentView.swift
@@ -14,25 +14,28 @@ struct ContentView: View {
         
         NavigationView {
             VStack{
-                TextField("heelo", text: $textSearch)
+                TextField("Searh here", text: $textSearch)
 //                    .background(Color.blue)
 //                    .cornerRadius(16)
                     .frame(width: .infinity, height: 30, alignment: .leading)
                     .padding(EdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8))
                     .font(.headline)
+                    
                     .overlay(
                         ZStack{
                             RoundedRectangle(cornerRadius: 5)
                                 .stroke(Color.gray.opacity(0.3), lineWidth: 1)
                                 .rectReader($bottomRect, in: .named("center"))
+                             
     
+                            
                             
                     
                             Image(uiImage: UIImage(systemName: "heart.fill")!)
                                 .frame(width: 39, height: 39, alignment: .trailing)
                                 .position(x: bottomRect.width - 39.0, y: bottomRect.height / 2)
                         }
-                    )
+                    ).background(RoundedRectangle(cornerRadius: 5).fill(Color.gray))
                     .padding(EdgeInsets(top: 16, leading: 16, bottom: 16, trailing: 16))
                     
                 
